f94d28df0a5b20f263da3f4c1b7776a1d03759b9
Umarkhon Akhmedov <umarkhonakhmedov2000@gmail.com>
2024-02-18 08:59:04 +0500

    Creating Code Generation Page and its apis

---
+++ app/api/code/route.ts
@@ -0,0 +1,48 @@
+interface ChatCompletionRequestMessage {
+  role: "user" | "assistant" | "system";
+  content: string;
+  name?: string;
+}
+
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const instructionMessage: ChatCompletionRequestMessage = {
+  role: "system",
+  content:
+    "You are a code genrator. You must answer only in markdown code snippets. Give some explanations.",
+};
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!openai) {
+      return new NextResponse("OpenAI API Key not configured", { status: 500 });
+    }
+
+    if (!messages) {
+      return new NextResponse("Messages are required", { status: 400 });
+    }
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [instructionMessage, ...messages],
+    });
+
+    return NextResponse.json(response.choices[0].message);
+  } catch (error) {
+    console.log("[Code_error]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
